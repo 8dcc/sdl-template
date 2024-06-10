0c49f9e3edce19477591a20acb753f739f0726ae
8dcc <8dcc.git@gmail.com>
2024-06-10 22:48:17 +0200

    Use cleaner format for switch statements

--- src/main.c
+++ src/main.c
@@ -71,19 +71,22 @@ int main(void) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
-                case SDL_QUIT:
+                case SDL_QUIT: {
                     running = false;
-                    break;
-                case SDL_KEYDOWN:
+                } break;
+
+                case SDL_KEYDOWN: {
                     switch (event.key.keysym.scancode) {
                         case SDL_SCANCODE_ESCAPE:
-                        case SDL_SCANCODE_Q:
+                        case SDL_SCANCODE_Q: {
                             running = false;
-                            break;
+                        } break;
+
                         default:
                             break;
                     }
-                    break;
+                } break;
+
                 default:
                     break;
             }
